c07927ccf334a1fea02b850d1b803c4b927ac4d2
edusilvaoli <edu.oli@gmail.com>
2021-03-21 22:34:50 -0300

    Criando operacao instanciao de categoria

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.example.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner { // com commandlinerunner pode adicionar os metodos na
+																// inicialiazação da app
+	@Autowired
+	private CategoriaRepository catRep; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria c1 = new Categoria(null,"Teste info");
+		Categoria c2 = new Categoria(null,"Teste Esc");
+		
+		catRep.saveAll(Arrays.asList(c1,c2)); 
 	}
 
 }
