85e8a6042a2898080dee854738be548857b4a8dd
edusilvaoli <edu.oli@gmail.com>
2021-03-24 22:15:47 -0300

    Associaçao produto e categoria muitos para muitos

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.domain.Produto;
 import com.example.cursomc.repositories.CategoriaRepository;
+import com.example.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner { // com commandlinerunner pode adicionar os metodos na
 																// inicialiazação da app
 	@Autowired
 	private CategoriaRepository catRep; 
+	
+	@Autowired
+	private ProdutoRepository prodRep; 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -26,7 +31,19 @@ public class CursomcApplication implements CommandLineRunner { // com commandlin
 		Categoria c1 = new Categoria(null,"Teste info");
 		Categoria c2 = new Categoria(null,"Teste Esc");
 		
+		Produto p1 = new Produto(null,"Cmputador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		c1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		c2.getProdutos().add(p2); 
+		
+		p1.getCategorias().add(c1);
+		p2.getCategorias().addAll(Arrays.asList(c1,c2)); 
+		p3.getCategorias().add(c1); 
+		
 		catRep.saveAll(Arrays.asList(c1,c2)); 
+		prodRep.saveAll(Arrays.asList(p1,p2,p3)); 
 	}
 
 }
--- src/main/java/com/example/cursomc/domain/Categoria.java
+++ src/main/java/com/example/cursomc/domain/Categoria.java
@@ -1,11 +1,16 @@
 package com.example.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.ManyToAny;
 
 @Entity
 public class Categoria implements Serializable {
@@ -24,6 +29,9 @@ public class Categoria implements Serializable {
 	private Long id; 
 	
 	private String nome; 
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() { //instancia objeto sem jogar nada com atributos
 		
@@ -75,6 +83,14 @@ public class Categoria implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 	
 	//comparar os objetos por valor
